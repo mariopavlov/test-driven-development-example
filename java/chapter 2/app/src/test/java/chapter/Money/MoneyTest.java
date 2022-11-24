4f293a3590c7d04709514c841a089b56b29958d2
Mario Pavlov <marioppavlov@gmail.com>
2022-11-24 10:43:40 +0200

    Chapter 2 - Start Point

---
+++ java/chapter 2/app/src/test/java/chapter/Money/MoneyTest.java
@@ -0,0 +1,14 @@
+package chapter.Money;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MoneyTest {
+
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
+}
