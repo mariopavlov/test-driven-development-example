2cfe3781ed688499f4fe89c5fdab7d55fb7a2bbc
Mario Pavlov <marioppavlov@gmail.com>
2022-11-24 10:42:51 +0200

    Chapter 1

---
+++ java/chapter-1/app/src/main/java/chapter/Money/Dollar.java
@@ -0,0 +1,13 @@
+package chapter.Money;
+
+public class Dollar {
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    public void times(int multiplier) {
+        amount *= multiplier;
+    }
+
+    int amount;
+}
