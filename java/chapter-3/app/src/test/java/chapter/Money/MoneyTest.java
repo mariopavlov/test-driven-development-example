dfcd04471c4f0964afa342a0f3cefb656a59ba4e
Mario Pavlov <marioppavlov@gmail.com>
2022-11-26 11:02:15 +0200

    Chapter 3 finished

---
+++ java/chapter-3/app/src/test/java/chapter/Money/MoneyTest.java
@@ -0,0 +1,22 @@
+package chapter.Money;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MoneyTest {
+
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        Dollar product = five.times(2);
+        assertEquals(10, product.amount);
+        product = five.times(3);
+        assertEquals(15, product.amount);
+    }
+
+    @Test
+    public void testEquality() {
+        assertEquals(new Dollar(5), new Dollar(5));
+        assertNotEquals(new Dollar(5), new Dollar(6));
+    }
+}
