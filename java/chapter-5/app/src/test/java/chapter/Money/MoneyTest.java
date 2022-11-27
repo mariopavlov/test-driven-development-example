011b8090c1d143447cb63bd497be5d53dff7dbe2
Mario Pavlov <marioppavlov@gmail.com>
2022-11-27 23:13:07 +0200

    Chapter 5 Initial code

---
+++ java/chapter-5/app/src/test/java/chapter/Money/MoneyTest.java
@@ -0,0 +1,20 @@
+package chapter.Money;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MoneyTest {
+
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
+    }
+
+    @Test
+    public void testEquality() {
+        assertEquals(new Dollar(5), new Dollar(5));
+        assertNotEquals(new Dollar(5), new Dollar(6));
+    }
+}
