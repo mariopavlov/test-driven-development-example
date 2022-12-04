a46de0b88cb8f43ad326976fc148950987b8bdeb
Mario Pavlov <marioppavlov@gmail.com>
2022-12-04 22:33:19 +0200

    Chapter 10 Final Code

---
+++ java/chapter-10/app/src/main/java/chapter/Money/Franc.java
@@ -0,0 +1,7 @@
+package chapter.Money;
+
+public class Franc extends Money {
+    public Franc(int amount, String currency) {
+        super(amount, currency);
+    }
+}
---
+++ java/chapter-10/app/src/test/java/chapter/Money/MoneyTest.java
@@ -0,0 +1,43 @@
+package chapter.Money;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MoneyTest {
+
+    @Test
+    public void testMultiplication() {
+        Money five = Money.dollar(5);
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
+    }
+
+    @Test
+    public void testEquality() {
+        assertEquals(Money.dollar(5), Money.dollar(5));
+        assertNotEquals(Money.dollar(5), Money.dollar(6));
+        assertEquals(Money.franc(5), Money.franc(5));
+        assertNotEquals(Money.franc(5), Money.franc(6));
+
+        assertNotEquals(Money.franc(5), Money.dollar(5));
+    }
+
+    @Test
+    public void testFrancMultiplication () {
+        Money five = Money.franc(5);
+        assertEquals(Money.franc(10), five.times(2));
+        assertEquals(Money.franc(15), five.times(3));
+    }
+
+    @Test
+    public void testCurrency() {
+        assertEquals("USD", Money.dollar(1).currency());
+        assertEquals("CHF", Money.franc(1).currency());
+    }
+
+    @Test
+    public void testDifferentClassEquality() {
+        assertEquals(new Money(10, "CHF"), new Franc(10, "CHF"));
+        assertEquals(new Money(10, "USD"), new Dollar(10, "USD"));
+    }
+}
